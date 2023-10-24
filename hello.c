16a47cfb1ec68fc5a2b1e5b7f16352e8d0e9234d
kh9504 <k.hiraizumi0417@gmail.com>
2023-10-25 06:12:33 +0900

    fix

--- hello.c
+++ hello.c
@@ -2,6 +2,23 @@
 #include <string.h>
 
 int main(void) {
-  string answer = "what's your name? ";
-  printf("hello, %s\n", answer);
+  // int n = 50;
+  // int *p = &n;
+
+  // printf("%i\n", n);
+  // printf("%p\n", p);
+
+  char *s = "Hi!";
+
+  printf("%s\n", s);
+  
+  printf("%p\n", &s[0]);
+  printf("%p\n", &s[1]);
+  printf("%p\n", &s[2]);
+  printf("%p\n", &s[3]);
+
+  printf("%c\n", *(s + 0));
+  printf("%c\n", *(s + 1));
+  printf("%c\n", *(s + 2));
+  printf("%c\n", *(s + 3));
 }
